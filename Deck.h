2127e630cc16f788c4fb8d8ede057754ae168d0e
AugustHjortholm <s215800@dtu.dk>
2022-05-05 04:04:35 +0200

    added random card shuffle

--- Deck.h
+++ Deck.h
@@ -1,4 +1,9 @@
 #include "Card.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void createDeck() {
     for (int i = 1; i < 14; ++i) {
@@ -13,8 +18,49 @@ void createDeck() {
 //Not sure if this needs to return the card or the node
 struct Node* removeTopCardOfDeck() {
     activeHead = 0;
-    struct Node* temp1 = getTail()->prev;
-    struct Node* temp2 = getTail();
-    temp1->next = NULL;
-    return temp2;
+    if (getTail()->prev != NULL) {
+        struct Node *temp1 = getTail()->prev;
+        struct Node *temp2 = getTail();
+        if (temp1->next != NULL) {
+            temp1->next = NULL;
+            return temp2;
+        }
+    }
+    return getTail();
+
+
+}
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void randomize(int arr[], int n) {
+    srand(time(NULL));
+    int i;
+    for(i = n-1; i > 0; i--) {
+        int j = rand() % (i+1);
+        swap(&arr[i], &arr[j]);
+    }
+}
+
+void shuffleDeckRandom(){
+    int pointerValueArray[52];
+    struct Node cardArray[52];
+    for (int i = 0; i < 52; ++i) {
+        cardArray[i] = *removeTopCardOfDeck();
+        pointerValueArray[i] = i;
+    }
+    randomize (pointerValueArray, 52);
+    addHead(&cardArray[pointerValueArray[0]],0);
+    for (int i = 1; i < 52; ++i) {
+        insertAtTail(cardArray[pointerValueArray[i]].data);
+    }
+}
+
+//not sure what interleaved shuffling is so
+void shuffleDeckInterleaved(){
+
 }

6ae488bc4d3413f7baed21897956f19f8299ba19
Cortex0101 <ldeiruff@gmail.com>
2022-05-06 23:30:18 +0200

    Cleanup main and change bg color

--- main.c
+++ main.c
@@ -39,28 +39,8 @@ int main( int argc, char* args[]) {
     print(0);
     loadCardTextureAndGenerateViews();
 
-    CardView card1 = getCard(1, Hearts);
-    CardView card2 = getCard(2, Clubs);
-    CardView card3 = getCard(3, Spades);
-    CardView card4 = getCard(4, Hearts);
-    CardView card5 = getCard(5, Clubs);
-    CardView card6 = getCard(6,Spades);
-    CardView card7 = getCard(7, Clubs);
-    CardView card8 = getCard(8, Hearts);
-    CardView card9 = getCard(9, Diamonds);
-    CardView card10 = getCard(10, Hearts);
-    CardView card11 = getCard(11, Diamonds);
-    CardView card12 = getCard(12, Clubs);
-    CardView card13 = getCard(13, Diamonds);
-
-    CardView column[13] = {card1, card2, card3, card4, card5, card6, card7, card8, card9, card10, card11, card12, card13};
-
-    /*Driver code to test the implementation*/
     setActiveList(1);
     initializeColumnLists();
-
-    moveCardsWithoutRules(2, 3, 5);
-    moveCardsWithoutRules(3, 4, 6);
 
     bool quit = false;
     SDL_Event e;
@@ -73,10 +53,7 @@ int main( int argc, char* args[]) {
             }
         }
 
-
-
-        //Clear screen
-        SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+        SDL_SetRenderDrawColor( gRenderer, 78, 106, 84, 0xFF );
         SDL_RenderClear( gRenderer );
 
         if (priv_column != 1 || !mouseButtonDown) drawColumn(1);
@@ -89,10 +66,7 @@ int main( int argc, char* args[]) {
 
         columnUpdateMouse();
 
-        //Update screen
         SDL_RenderPresent( gRenderer );
-
-
     }
 
     closeWindow();

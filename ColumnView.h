daa99403ce5860264a0886f80cb3d39b899ec7b0
Cortex0101 <ldeiruff@gmail.com>
2022-05-09 09:13:12 +0200

    Cards now become visible if they are at the tail

--- ColumnView.h
+++ ColumnView.h
@@ -29,6 +29,9 @@ void drawColumn(int column) {
             cardView = getCardView(temp->data);
             cardView.xPos = (COLUMN_HORIZONTAL_SPACING * column) + COLUMN_X;
             cardView.yPos = (COLUMN_VERTICAL_SPACING * i) + COLUMN_Y;
+            if (temp->next == NULL && !temp->data.visible) {
+                temp->data.visible = true;
+            }
             drawCard(&cardView);
             ++i;
         } else {
